fbb2f934f63f57ea95c5b81e32e626d6397aa58c
Andrey <Kuksa>
2021-04-07 21:19:13 +0300

    Added client server chat

--- client/src/main/kotlin/com/jetbrains/handson/chat/client/ChatClient.kt
+++ client/src/main/kotlin/com/jetbrains/handson/chat/client/ChatClient.kt
@@ -5,9 +5,49 @@ import io.ktor.client.features.websocket.*
 import io.ktor.http.*
 import io.ktor.http.cio.websocket.*
 import io.ktor.util.*
-import kotlinx.coroutines.*
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 @KtorExperimentalAPI
 fun main() {
+    val client = HttpClient {
+        install(WebSockets)
+    }
+    runBlocking {
+        client.webSocket(method = HttpMethod.Get, host = "127.0.0.1", port = 8080, path = "/chat") {
+            val messageOutputRoutine = launch { outputMessages() }
+            val userInputRoutine = launch { inputMessages() }
 
+            userInputRoutine.join()
+            messageOutputRoutine.cancelAndJoin()
+        }
+    }
+    client.close()
+    println("Connection close")
+}
+
+suspend fun DefaultWebSocketSession.inputMessages() {
+    try {
+        for (message in incoming) {
+            message as? Frame.Text ?: continue
+            println(message.readText())
+        }
+    } catch (e: Exception){
+        println("Error while receiving " + e.localizedMessage)
+    }
+}
+
+suspend fun DefaultWebSocketSession.outputMessages() {
+    while (true) {
+        val message = readLine() ?: ""
+        if ("exit".equals(message, true))
+            return
+        try {
+            send(message)
+        } catch (e: Exception) {
+            println("Error during sending message " + e.localizedMessage)
+            return
+        }
+    }
 }
--- server/src/main/kotlin/com/jetbrains/handson/chat/server/Application.kt
+++ server/src/main/kotlin/com/jetbrains/handson/chat/server/Application.kt
@@ -5,11 +5,38 @@ import io.ktor.http.cio.websocket.*
 import io.ktor.routing.*
 import io.ktor.websocket.*
 import java.util.*
+import kotlin.collections.LinkedHashSet
 
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
-@Suppress("unused")
 fun Application.module() {
+    install(WebSockets)
+    routing {
+        val connections = Collections.synchronizedSet<Connection?>(LinkedHashSet())
+        webSocket("/chat") {
+            send("Adding the user!")
+            val currentConnection = Connection(this)
+            connections += currentConnection
+            try{
+                send("You are connected. There are ${connections.count()} users here.")
+                for(frame in incoming){
+                    frame as? Frame.Text ?:continue
+                    val receivedText = frame.readText()
+                    val textUsername = "[${currentConnection.name}] : $receivedText"
+                    connections.forEach{
+                        it.session.send(textUsername)
+                    }
 
+                }
+            } catch (e: Exception){
+                println(e.localizedMessage)
+            } finally {
+                println("Removing ${currentConnection.name}")
+                connections -= currentConnection
+            }
+        }
+    }
 }
+
+
